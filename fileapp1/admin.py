97c6e308176f843fb489d77c97290b8081013c83
prasadbabu <prasadbabu247@gmail.com>
2019-05-30 13:13:02 +0530

    first commit

---
+++ fileapp1/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from fileapp1.models import Book
+admin.site.register(Book)
---
+++ fileapp1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Fileapp1Config(AppConfig):
+    name = 'fileapp1'
